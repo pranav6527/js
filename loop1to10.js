c5ecd381343963750c93dfea6cd6fa204bf6dd3f
Pranav Dusane <pranav6527@gmail.com>
2026-01-21 00:28:40 +0530

    added new line to c

--- loop1to10.js
+++ loop1to10.js
@@ -3,4 +3,5 @@ for (let i = 1; i <= 10; i++) {
   console.log(i + "from new b");
   console.log("again added new b");
   console.log("this line in a new one");
+  console.log("again added new in c");
 }

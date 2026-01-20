7ea7a29299383188e6155a9e05425a7646005c0f
Pranav Dusane <pranav6527@gmail.com>
2026-01-21 00:31:27 +0530

    new line added to b

--- loop1to10.js
+++ loop1to10.js
@@ -1,5 +1,5 @@
 for (let i = 1; i <= 10; i++) {
   console.log("i am new main");
-  console.log(i + "from c");
-  console.log(i + "from c and chaged this");
+  console.log(i + "from new b");
+  console.log(i + "from a");
 }
